69008a03e345ae50157b1d18474cef43574e96fb
francescofalcone24 <cicciofalcon97@gmail.com>
2025-06-28 11:06:11 +0200

    added products

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -21,5 +21,221 @@ class ProductSeeder extends Seeder
         $newProduct->size_ml = 100;
         $newProduct->img = "img/dior.webp";
         $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Giorgio Armani";
+        $newProduct->model = "Acqua Di Giò";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "49,90";
+        $newProduct->size_ml = 100;
+        $newProduct->img = "img/acquadigio.jpg";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Tom Ford";
+        $newProduct->model = "Tobacco Vanille";
+        $newProduct->type = "Eau de parfum";
+        $newProduct->price = "79,99";
+        $newProduct->size_ml = 50;
+        $newProduct->img = "img/TOM-FORD.webp";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "David Off";
+        $newProduct->model = "Cool Water";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "49,99";
+        $newProduct->size_ml = 100;
+        $newProduct->img = "img/davidoff.jpg";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Bvlgari";
+        $newProduct->model = "Rain Essence";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "39,99";
+        $newProduct->size_ml = 50;
+        $newProduct->img = "img/bulgari.avif";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Dior";
+        $newProduct->model = "Sauvage";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "59,99";
+        $newProduct->size_ml = 100;
+        $newProduct->img = "img/dior.webp";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Giorgio Armani";
+        $newProduct->model = "Acqua Di Giò";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "49,90";
+        $newProduct->size_ml = 100;
+        $newProduct->img = "img/acquadigio.jpg";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Tom Ford";
+        $newProduct->model = "Tobacco Vanille";
+        $newProduct->type = "Eau de parfum";
+        $newProduct->price = "79,99";
+        $newProduct->size_ml = 50;
+        $newProduct->img = "img/TOM-FORD.webp";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "David Off";
+        $newProduct->model = "Cool Water";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "49,99";
+        $newProduct->size_ml = 100;
+        $newProduct->img = "img/davidoff.jpg";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Bvlgari";
+        $newProduct->model = "Rain Essence";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "39,99";
+        $newProduct->size_ml = 50;
+        $newProduct->img = "img/bulgari.avif";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Dior";
+        $newProduct->model = "Sauvage";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "59,99";
+        $newProduct->size_ml = 100;
+        $newProduct->img = "img/dior.webp";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Giorgio Armani";
+        $newProduct->model = "Acqua Di Giò";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "49,90";
+        $newProduct->size_ml = 100;
+        $newProduct->img = "img/acquadigio.jpg";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Tom Ford";
+        $newProduct->model = "Tobacco Vanille";
+        $newProduct->type = "Eau de parfum";
+        $newProduct->price = "79,99";
+        $newProduct->size_ml = 50;
+        $newProduct->img = "img/TOM-FORD.webp";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "David Off";
+        $newProduct->model = "Cool Water";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "49,99";
+        $newProduct->size_ml = 100;
+        $newProduct->img = "img/davidoff.jpg";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Bvlgari";
+        $newProduct->model = "Rain Essence";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "39,99";
+        $newProduct->size_ml = 50;
+        $newProduct->img = "img/bulgari.avif";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Dior";
+        $newProduct->model = "Sauvage";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "59,99";
+        $newProduct->size_ml = 100;
+        $newProduct->img = "img/dior.webp";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Giorgio Armani";
+        $newProduct->model = "Acqua Di Giò";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "49,90";
+        $newProduct->size_ml = 100;
+        $newProduct->img = "img/acquadigio.jpg";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Tom Ford";
+        $newProduct->model = "Tobacco Vanille";
+        $newProduct->type = "Eau de parfum";
+        $newProduct->price = "79,99";
+        $newProduct->size_ml = 50;
+        $newProduct->img = "img/TOM-FORD.webp";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "David Off";
+        $newProduct->model = "Cool Water";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "49,99";
+        $newProduct->size_ml = 100;
+        $newProduct->img = "img/davidoff.jpg";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Bvlgari";
+        $newProduct->model = "Rain Essence";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "39,99";
+        $newProduct->size_ml = 50;
+        $newProduct->img = "img/bulgari.avif";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Dior";
+        $newProduct->model = "Sauvage";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "59,99";
+        $newProduct->size_ml = 100;
+        $newProduct->img = "img/dior.webp";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Giorgio Armani";
+        $newProduct->model = "Acqua Di Giò";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "49,90";
+        $newProduct->size_ml = 100;
+        $newProduct->img = "img/acquadigio.jpg";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Tom Ford";
+        $newProduct->model = "Tobacco Vanille";
+        $newProduct->type = "Eau de parfum";
+        $newProduct->price = "79,99";
+        $newProduct->size_ml = 50;
+        $newProduct->img = "img/TOM-FORD.webp";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "David Off";
+        $newProduct->model = "Cool Water";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "49,99";
+        $newProduct->size_ml = 100;
+        $newProduct->img = "img/davidoff.jpg";
+        $newProduct->save();
+
+        $newProduct = new Product();
+        $newProduct->brand = "Bvlgari";
+        $newProduct->model = "Rain Essence";
+        $newProduct->type = "Eau de toilette";
+        $newProduct->price = "39,99";
+        $newProduct->size_ml = 50;
+        $newProduct->img = "img/bulgari.avif";
+        $newProduct->save();
     }
 }
