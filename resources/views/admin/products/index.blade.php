fcf1b14c9044724302cfaa86ec6a6254fb45bebb
francescofalcone24 <cicciofalcon97@gmail.com>
2025-07-01 12:29:05 +0200

    cleaning code

--- resources/views/admin/products/index.blade.php
+++ resources/views/admin/products/index.blade.php
@@ -1,53 +1,8 @@
 @extends('layouts.admin')
 
 @section('content')
-	{{-- <div class="d-flex justify-content-center flex-wrap">
-		@foreach ($products as $product)
-			<div class="card m-2 p-2 text-center" style="width: 18rem;">
-				<div class="card-body">
-					<h5 class="card-title">Product: {{ $product->brand }}</h5>
-					<p>Model: {{ $product->model }}</p>
-					<a href="{{ route('admin.products.show', $product->id) }}" class="btn btn-primary">more details</a>
-					<button type="button" class="btn btn-outline-danger" data-bs-toggle="modal"
-						data-bs-target="#modal-{{ $product->id }}">
-						<i class="fa-solid fa-trash-can"></i>
-					</button>
-					<!-- Modal Body -->
-					<!-- if you want to close by clicking outside the modal, delete the last endpoint:data-bs-backdrop and data-bs-keyboard -->
-					<div class="modal fade" id="modal-{{ $product->id }}" tabindex="-1" data-bs-backdrop="static"
-						data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitle-{{ $product->id }}" aria-hidden="true">
-						<div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-sm" role="document">
-							<div class="modal-content">
-								<div class="modal-header">
-									<h5 class="modal-title" id="modalTitle-{{ $product->id }}">
-										Delete current product
-									</h5>
-									<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-								</div>
-								<div class="modal-body">
-									Deleting {{ $product->brand }}-{{ $product->model }}
-									⚠️ you cannot undo this operation
-								</div>
-								<div class="modal-footer justify-content-center">
-									{{-- CREO FORM PER CANCELLARE UN FUMETTO DAL DATABASE GLI DO LA ROTTA DESTROY E IL METODO POST POI SOTTO LO CAMBIO NEL METODO DELETE --}}
-	{{-- <form method="POST" action="{{ route('admin.products.destroy', $product->id) }}">
-										@csrf
-										@method('DELETE')
-										<button type="submit" href="" class="btn btn-outline-danger my-2" data-bs-dismiss="modal">
-											<i class="fa-solid fa-trash-can"></i>
-										</button>
-									</form>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		@endforeach
-	</div> --}}
 	<table class="styled-table w-100">
 		<thead class="">
-
 			<th>IMAGE</th>
 			<th>BRAND</th>
 			<th>MODEL</th>
