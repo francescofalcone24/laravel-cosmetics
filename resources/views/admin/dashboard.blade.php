836fcc41725ca4b954bd27be160bbee1cca8006b
francescofalcone24 <cicciofalcon97@gmail.com>
2025-07-02 22:40:33 +0200

    dashboard table added

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,23 +1,18 @@
 @extends('layouts.admin')
 
 @section('content')
-    <div class="container-fluid mt-4">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+	<div class="container-fluid mt-4">
+		<div class="row justify-content-center">
+			<div class="col-md-8">
+				<div class="card">
+					<div class="text-center card-header">{{ __('Dashboard') }}</div>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
+					<div class="card-body p-0">
 
-                        {{ __('You are logged in!') }}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+						<img style="width: 100%" src="{{ asset('/img/dashboard.webp') }}" alt="">
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
